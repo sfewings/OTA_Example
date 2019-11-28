293f903fe54a599ef976683a7826084559fbf0d0
sfewings3 <stephen@penningtonscott.com.au>
2019-11-28 17:10:04 +0800

    Changes to main. A new version should be downloaded

--- main/main.py
+++ main/main.py
@@ -3,12 +3,10 @@
 #
 import picoweb
 
-from ota_update.main.ota_updater import OTAUpdater
+from ota_updater import OTAUpdater
 
 import time
 import network
-
-
 
 
 def index(req, resp):
@@ -19,20 +17,36 @@ def index(req, resp):
     yield from resp.awrite("\r\n")
     yield from resp.awrite("<h2>Pyboard running Picoweb</h2>")
     yield from resp.awrite("<ul style=""list-style-type:circle;"">")
-    yield from resp.awrite("<li>Check for updates<a href='CheckForUpdate'>Check</a>.</li>")
-    yield from resp.awrite("<li>source file of main.py <a href='file'>source</a>.</li>")
+    yield from resp.awrite("<li>Check for updates<a href='CheckForUpdates'>Check</a>.</li>")
+    yield from resp.awrite("<li>Install updates, if available<a href='InstallUpdates'>Install Updates</a>.</li>")   
+    yield from resp.awrite("<li>source file of main.py <a href='source'>source</a>.</li>")
     yield from resp.awrite("</ul>")
- 
 
-@app.route
+
+
+ROUTES = [
+  ("/", index),
+      # You can specify exact URI string matches...
+]
+
+app = picoweb.WebApp(__name__, ROUTES)
+
+
+@app.route("/CheckForUpdates")
 def CheckForUpdates(req, resp):
-    o = OTAUpdater()
-    o.download_and_install_update_if_available('wifi-ssid', 'wifi-password')
-    yield from app.sendfile(resp, 'static/Example.html')
+    o = OTAUpdater("https://github.com/sfewings/OTA_Example",main_dir=".")
+    o.check_for_update_to_install_during_next_reboot()
+    
+    
+@app.route("/InstallUpdates")
+def installUpdates(req,resp):
+    o = OTAUpdater("https://github.com/sfewings/OTA_Example",main_dir=".")
+    o.download_and_install_update_if_available("","")
 
-@app.route
+
+@app.route("/source")
 def source(req, resp):
-    yield from app.sendfile(resp, 'main/main.py')
+    yield from app.sendfile(resp, 'main.py')
 
 #### Parsing function
 
@@ -55,15 +69,12 @@ def GetIP() :
         print("unable to connect to wifi")
     return sta_if.ifconfig()[0]
 
-ROUTES = [
-    # You can specify exact URI string matches...
-]
+
 
 import ulogging as logging
 logging.basicConfig(level=logging.INFO)
 
 
-app = picoweb.WebApp(__name__, ROUTES)
 
 """
 async def my_dns_server(args):
